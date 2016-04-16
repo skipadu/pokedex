74240f7eb88ca2bdda770e35867b597ac07d9793
Sami Korpela <sami.korpela@gmail.com>
2016-04-16 10:32:47 +0300

    Initial Commit

---
+++ pokedex-by-skipadu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-skipadu
+//
+//  Created by Sami Korpela on 16.4.2016.
+//  Copyright © 2016 skipadu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
