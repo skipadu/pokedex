3526e6440b912f1d858f246af67016139e52b640
skipadu <skipadu@users.noreply.github.com>
2016-04-16 12:19:15 +0300

    Showing all pokemons in UICollectionView

--- pokedex-by-skipadu/ViewController.swift
+++ pokedex-by-skipadu/ViewController.swift
@@ -10,7 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var collection: UICollectionView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    collection.delegate = self
+    collection.dataSource = self
   }
+}
+
+// MARK: - UICollectionViewDelegate
+extension ViewController: UICollectionViewDelegate {
+  
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+  }
+  
+}
+
+// MARK: - UICollectionViewDataSource
+extension ViewController: UICollectionViewDataSource {
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell {
+      
+      let pokemon = Pokemon(name: "Test", pokedexId: indexPath.row)
+      cell.configureCell(pokemon)
+      
+      return cell
+    } else {
+      return UICollectionViewCell()
+    }
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 718
+  }
+  
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ViewController: UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    return CGSizeMake(105, 105)
+  }
+  
 }
