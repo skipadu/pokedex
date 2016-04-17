2cb7b447cb1491d831f6c292d6a4836edcd01a33
skipadu <skipadu@users.noreply.github.com>
2016-04-17 13:28:36 +0300

    Made outlets for detailsview and variables to Pokemon class

--- pokedex-by-skipadu/Pokemon.swift
+++ pokedex-by-skipadu/Pokemon.swift
@@ -11,6 +11,13 @@ import Foundation
 class Pokemon {
   private var _name: String!
   private var _pokeDexId: Int!
+  private var _description: String!
+  private var _type: String!
+  private var _defense: String!
+  private var _height: String!
+  private var _weight: String!
+  private var _attack: String!
+  private var _nextEvolutionTxt: String!
   
   var name: String {
     return _name
--- pokedex-by-skipadu/PokemonDetailVC.swift
+++ pokedex-by-skipadu/PokemonDetailVC.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
+  @IBOutlet weak var nameLbl: UILabel!
+  @IBOutlet weak var mainImg: UIImageView!
+  @IBOutlet weak var descLbl: UILabel!
+  @IBOutlet weak var typeLbl: UILabel!
+  @IBOutlet weak var defenseLbl: UILabel!
+  @IBOutlet weak var heightLbl: UILabel!
+  @IBOutlet weak var pokedexIdLbl: UILabel!
+  @IBOutlet weak var weightLbl: UILabel!
+  @IBOutlet weak var baseAttackLbl: UILabel!
+  
+  @IBOutlet weak var evoLbl: UILabel!
+  @IBOutlet weak var currentEvoImg: UIImageView!
+  @IBOutlet weak var nextEvoImg: UIImageView!
   
   var pokemon: Pokemon!
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  @IBAction func backButtonPressed(sender: AnyObject) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
 }
