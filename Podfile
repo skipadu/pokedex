85427bf04ff31956921fd3b6182e1e421f937568
skipadu <skipadu@users.noreply.github.com>
2016-04-17 13:48:07 +0300

    Added AlamoFire pod file

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'pokedex-by-skipadu' do
+pod 'Alamofire', '~> 3.3'
+
+end
+
---
+++ Pods/Target Support Files/Pods-pokedex-by-skipadu/Pods-pokedex-by-skipadu-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_pokedex_by_skipaduVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pokedex_by_skipaduVersionString[];
+
