097f4d814a8058a66f3221eedb76d895bef8fb9d
skipadu <skipadu@users.noreply.github.com>
2016-04-17 16:16:06 +0300

    Added StringUtils to help with nil strings

---
+++ pokedex-by-skipadu/StringUtils.swift
@@ -0,0 +1,17 @@
+//
+//  StringUtils.swift
+//  pokedex-by-skipadu
+//
+//  Created by Sami Korpela on 17.4.2016.
+//  Copyright © 2016 skipadu. All rights reserved.
+//
+
+import Foundation
+
+func returnEmptyIfNil(string: String?) -> String {
+  var result: String? = string
+  if string == nil {
+    result = ""
+  }
+  return result!
+}
